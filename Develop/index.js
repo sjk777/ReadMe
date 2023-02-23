4d7fd156702b4d6c8a09279c95b5ba2e950612c6
Sky K <skyj.kim7@gmail.com>
2023-02-23 12:44:55 -0800

    added all requirements, and inputting questions templates

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,26 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
+const path = require('path');
+const generateMarkdown = require('./utils/generateMarkdown')
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'repoLink',
+        message: 'Enter Repo Link:',
+    },
+    {
+        type:'input',
+        name:'liveLink',
+        message: 'Enter your Live Link:',
+    },
+    {
+        type: 'input',
+        name:'liveLink',
+        message: 'Enter the title of your Project:',
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
