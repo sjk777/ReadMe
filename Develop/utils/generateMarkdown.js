bea0a18d1801cc7cb54f34f9717cd02bb3bc0c62
Sky K <skyj.kim7@gmail.com>
2023-02-23 14:07:44 -0800

    created function that returns the license section of ReadME

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -13,11 +13,26 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if(license === "none"){
+    return "";
+  }
+  else{
+    return "-[License](#license)"
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if(license === "none"){
+    return "";
+  }
+  else{
+    return `## License
+    For more information on this license, please visit www.opensource.org`;
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
