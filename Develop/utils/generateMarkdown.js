5d213908e697b35ceb9e280203e59e166e040276
Sky K <skyj.kim7@gmail.com>
2023-02-23 14:05:08 -0800

    added function that returns an empty string when there is no license

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,15 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
+const https = require('https');
+
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if(license === "none"){
+    return "";
+  }
+  else{
+    return `![github license](https://img.shields.io/badge/license-${license}.svg)`
+  }
+};
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
